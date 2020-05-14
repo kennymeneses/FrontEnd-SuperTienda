392228003f8089f0915001695d9ba3bbe42509ec
Kenny Meneses <kennny16m@gmail.com>
2020-05-14 14:48:59 -0500

    Se agrego redux-devtools-extensions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,6 @@ import logo from '../logo.svg';
 import '../App.css';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-
 const App = ()=>(
     <div className="App">
       <header className="App-header">
