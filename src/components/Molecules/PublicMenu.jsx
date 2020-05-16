6df83262c1b186a01e7ef3500c88c5606880db8f
Kenny Meneses <kennny16m@gmail.com>
2020-05-15 19:49:00 -0500

    se empeze a desarrollar el header y header responsive

--- src/components/Molecules/PublicMenu.jsx
+++ src/components/Molecules/PublicMenu.jsx
@@ -6,9 +6,21 @@ const PublicMenu = () =>{
 
     return (
         <ul className="display-menu">
-            <li className="display-menu-item"><NavLink className="link-nav" exact to="/">Home</NavLink></li>
-            <li className="display-menu-item"><NavLink className="link-nav" exact to="/">Home</NavLink></li>
-            <li className="display-menu-item"><NavLink className="link-nav" exact to="/">Home</NavLink></li>
+            <li className="display-menu-item">
+                <NavLink className="link-nav" exact to="/">
+                    <img className="logo-usuario"
+                         src="https://giftcards.woolworths.com.au/_ui/responsive/common/images/User_icon_white.png" 
+                         alt="logo_del_usuario"/>
+                </NavLink>
+            </li>
+
+            <li className="display-menu-item">
+                <NavLink className="link-nav" exact to="/">
+                    <img className="logo-cart"
+                         src="https://pluspng.com/img-png/shop-png-black-and-white--512.png" 
+                         alt="logo_del_carrito"/>
+                </NavLink>
+            </li>
         </ul>
 
     )
